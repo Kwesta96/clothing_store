c05bc9d4f8afd0488b6b042b0e795e5647781073
Paa Kwesi Owusu-Ansah <kwesta96@gmail.com>
2022-03-04 15:05:48 +0200

    changes

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -84,9 +84,9 @@ export default function Slider() {
     const handleClick = (direction) => {
 
         if (direction === "left") {
-            setSlideIndex(slideIndex > 0 ? slideIndex-1 : 2);
+            setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
         } else {
-            setSlideIndex(slideIndex < 2 ? slideIndex+1 : 0);
+            setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
         }
     };
 
